0a5e432ed373d672515049d234b2ac095942add7
Abd-ELrahmanHamza <68310502+Abd-ELrahmanHamza@users.noreply.github.com>
2022-03-27 21:43:48 +0200

    remove stop words

--- src/SearchPackage/Indexer.java
+++ src/SearchPackage/Indexer.java
@@ -1,13 +1,19 @@
 package SearchPackage;
-import  java.util.*;
-import  java.io.*;
+
+import java.util.*;
+import java.io.*;
+
 import org.jsoup.Jsoup;
+
 import java.util.regex.Pattern;
 import java.util.regex.Matcher;
+
 import org.jsoup.nodes.Document;
 
 public class Indexer {
-    public static void main(String[]args){
+    List<String> stopWords;
+
+    public static void main(String[] args) {
 //        String s=Parsing("../input.txt");
 //        Vector<String> ll;
 //        ll= splitWords(s);
@@ -15,9 +21,10 @@ public class Indexer {
 //            System.out.println(ll.get(i));
 //        }
     }
-    public static String Parsing(String input){
+
+    public static String Parsing(String input) {
         String lines = "";
-        StringBuilder Str=new StringBuilder();
+        StringBuilder Str = new StringBuilder();
         try {
             BufferedReader reader = new BufferedReader(new FileReader(input));
 
@@ -25,21 +32,36 @@ public class Indexer {
                 Str.append(lines);
             }
             reader.close();
-            lines=Str.toString();
-            Document html= Jsoup.parse(lines);
-            lines= html.text();
+            lines = Str.toString();
+            Document html = Jsoup.parse(lines);
+            lines = html.text();
+        } catch (IOException e) {
+            e.printStackTrace();
         }
-            catch (IOException e){
-                e.printStackTrace();}
         return lines;
     }
-    public static Vector<String> splitWords(String Lines){
-        Vector<String> words =new<String> Vector();
+
+    public static Vector<String> splitWords(String Lines) {
+        Vector<String> words = new <String>Vector();
         Pattern pattern = Pattern.compile("\\w+");
         Matcher match = pattern.matcher(Lines);
-        while (match.find()){
+        while (match.find()) {
             words.add(match.group());
         }
         return words;
+    }
+
+
+    private void readStopWords() throws IOException {
+        BufferedReader reader = new BufferedReader(new FileReader("stopwords.txt"));
+        stopWords = new Vector<String>();
+        String word;
+        while ((word = reader.readLine()) != null) {
+            stopWords.add(word);
+        }
+    }
+
+    private void removeStopWords(List<String> words) {
+        words.removeAll(stopWords);
     }
 }
