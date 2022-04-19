262e0bad8e6427ccf48df13d3579e98237a45044
ZiadSheriif <zsherif308@gmail.com>
2022-04-19 20:37:23 +0200

    QP

---
+++ src/SearchPackage/queryProcessing.java
@@ -0,0 +1,16 @@
+package SearchPackage;
+
+import java.util.List;
+
+public class queryProcessing {
+    private static List<String> stopWords;
+
+    public static void main(String[] args) {
+
+    }
+    // TODO
+    // 1) split-words
+    // 2) remove stop words
+    // 3) stemming
+    // 4) Retrieve all docs in DB
+}
