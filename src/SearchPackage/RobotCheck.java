85f680025535d74b7df8e8be78197464da6d947c
Zeyad Tarek <zeyad.ta01@gmail.com>
2022-04-19 16:19:53 +0200

    Editing the robot datastructure and function

--- src/SearchPackage/RobotCheck.java
+++ src/SearchPackage/RobotCheck.java
@@ -4,14 +4,17 @@ import java.io.IOException;
 import java.net.URL;
 import java.util.ArrayList;
 import java.io.DataInputStream;
+import java.util.HashMap;
 import java.util.List;
 import java.io.FileWriter;
 
 
 public class RobotCheck {
-    ArrayList<String> DisallowedLinks = new ArrayList<String>();
 
-    private void getPageLinks(String newURL) {
+    HashMap<String, ArrayList<String>> allDisallowedLinks = new HashMap<>();
+    ArrayList<String> disallowed;
+
+    private void getPageDisallows(String newURL) {
         boolean userAgentStatus = false;
         URL url = null;
         String protocol = null;
@@ -27,6 +30,12 @@ public class RobotCheck {
         }
         StringBuilder robotCommands = new StringBuilder();
         if (url != null) {
+            if (allDisallowedLinks.get(url.toString()) != null) {
+                System.out.println(url.toString() + " Found");
+                return;
+            }
+            System.out.println(url.toString() + " New");
+            disallowed = new ArrayList<>();
             try {
                 String readLine;
 
@@ -38,59 +47,62 @@ public class RobotCheck {
             } catch (IOException e) {
                 System.err.println("Error happened while trying to read the content of  '" + newURL + "': " + e.getMessage());
             }
-        }
 
-        String[] arrOfWords = robotCommands.toString().split("\n");
-        for (String arrOfWord : arrOfWords) {
-            String word = arrOfWord.trim();
-            if (word.startsWith("User-agent:")) {
-                int indexOfChar = word.indexOf(':') + 2;
-                userAgentStatus = word.charAt(indexOfChar) == '*';
-            } else if (word.startsWith("Disallow:") && userAgentStatus) {
-                String disallowedDirectories = word.substring(10).trim();
-                String disallowedUrl = protocol + "://" + serverName + disallowedDirectories;
-                DisallowedLinks.add(disallowedUrl);
+
+            String[] arrOfWords = robotCommands.toString().split("\n");
+            for (String arrOfWord : arrOfWords) {
+                String word = arrOfWord.trim();
+                if (word.startsWith("User-agent:")) {
+                    int indexOfChar = word.indexOf(':') + 2;
+                    userAgentStatus = word.charAt(indexOfChar) == '*';
+                } else if (word.startsWith("Disallow:") && userAgentStatus) {
+                    String disallowedDirectories = word.substring(10).trim();
+                    String disallowedUrl = protocol + "://" + serverName + disallowedDirectories;
+                    disallowed.add(disallowedUrl);
+                }
             }
+
+            allDisallowedLinks.put(url.toString(), disallowed);
         }
-
-
     }
 
     public void generateDisallowedLinks(List<String> pagesToVisit) {
         for (String s : pagesToVisit) {
-            getPageLinks(s);
+            getPageDisallows(s);
         }
     }
 
     public void testing(List<String> pagesToVisit) {
         generateDisallowedLinks(pagesToVisit);
         printDisallowedURLs();
-        writeDisallowedURLsToFile("testing");
+//        writeDisallowedURLsToFile("testing");
     }
 
     public void printDisallowedURLs() {
-        for (String disallowedLink : DisallowedLinks) {
-            System.out.println(disallowedLink);
+        for (String key : allDisallowedLinks.keySet()) {
+            System.out.print(key);
+            System.out.println(allDisallowedLinks.get(key));
         }
     }
 
-    public void writeDisallowedURLsToFile(String fileName) {
-        try {
-            FileWriter myObject = new FileWriter(fileName + ".txt");
-            for (String disallowedLink : DisallowedLinks) {
-                myObject.write(disallowedLink);
-                myObject.write("\n");
-            }
-            myObject.close();
-        } catch (IOException e) {
-            System.out.println("An error occurred! File can't be created.");
-        }
-
-    }
+//    public void writeDisallowedURLsToFile(String fileName) {
+//        try {
+//            FileWriter myObject = new FileWriter(fileName + ".txt");
+//            for (String disallowedLink : DisallowedLinks) {
+//                myObject.write(disallowedLink);
+//                myObject.write("\n");
+//            }
+//            myObject.close();
+//        } catch (IOException e) {
+//            System.out.println("An error occurred! File can't be created.");
+//        }
+//
+//    }
 
     public static void main(String[] args) {
         RobotCheck obj = new RobotCheck();
         List<String> links = new ArrayList<String>();
+        links.add("https://www.google.com/");
         links.add("https://www.google.com/");
         links.add("https://github.com/");
         links.add("https://www.geeksforgeeks.org/");
