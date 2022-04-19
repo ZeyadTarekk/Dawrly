43935561cb9af7fb6fccb6dbed09fe158a361b51
ZiadSheriif <zsherif308@gmail.com>
2022-04-19 22:19:38 +0200

    Process String (Abstract)

--- src/SearchPackage/ProcessString.java
+++ src/SearchPackage/ProcessString.java
@@ -1,4 +1,60 @@
 package SearchPackage;
 
+import org.tartarus.snowball.ext.PorterStemmer;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.List;
+import java.util.Vector;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class ProcessString {
+    private static List<String> stopWords;
+
+    public static void main(String[] args) {
+
+    }
+
+    protected static List<String> splitWords(String Lines) {
+        List<String> words = new <String>Vector();
+        Pattern pattern = Pattern.compile("\\w+");
+        Matcher match = pattern.matcher(Lines);
+        while (match.find()) {
+            words.add(match.group());
+        }
+        return words;
+    }
+
+    protected static void readStopWords() throws IOException {
+        BufferedReader reader = new BufferedReader(new FileReader("stopwords.txt"));
+        stopWords = new Vector<String>();
+        String word;
+        while ((word = reader.readLine()) != null) {
+            stopWords.add(word);
+        }
+    }
+
+    protected static void convertToLower(List<String> temp) {
+        for (int i = 0; i < temp.size(); i++) {
+            temp.set(i, temp.get(i).toLowerCase());
+        }
+    }
+
+    protected static void removeStopWords(List<String> words) {
+        words.removeAll(stopWords);
+    }
+
+    protected static List<String> stemming(List<String> words) {
+        PorterStemmer stemmer = new PorterStemmer();
+        // stem words in the list
+        for (int i = 0; i < words.size(); i++) {
+            stemmer.setCurrent(words.get(i)); //set string you need to stem
+            stemmer.stem();  //stem the word
+            words.set(i, stemmer.getCurrent()); //get the stemmed word
+        }
+        return words;
+    }
+
 }
