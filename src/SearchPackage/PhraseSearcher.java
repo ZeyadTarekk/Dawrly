714b678bda1e320c1867c53123376e592f23000d
ZiadSheriif <zsherif308@gmail.com>
2022-04-20 00:16:00 +0200

    Phrase Searcher

---
+++ src/SearchPackage/PhraseSearcher.java
@@ -0,0 +1,11 @@
+package SearchPackage;
+
+public class PhraseSearcher {
+
+    // TODO: Provide an interface to receive the query string
+    // TODO: Open each file and search for this query
+    // TODO: Provide an interface which returns list of file names
+    //   "manga is good"
+
+
+}
