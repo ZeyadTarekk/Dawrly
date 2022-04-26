87cbec9b6b6b86f8739f7a2e7bb0443b54069d2b
Abd-ELrahmanHamza <68310502+Abd-ELrahmanHamza@users.noreply.github.com>
2022-04-26 23:00:11 +0200

    PhraseSearcher for list of indices

--- src/SearchPackage/MainClass.java
+++ src/SearchPackage/MainClass.java
@@ -7,21 +7,21 @@ import java.util.List;
 public class MainClass {
     public static void main(String[] args) throws InterruptedException {
 //        System.out.println("Zeyad");
-        Indexer indexer = new Indexer();
-        indexer.startIndexing();
+//        Indexer indexer = new Indexer();
+//        indexer.startIndexing();
 //
         List<String> phraseSearch = new ArrayList<>();
 //        // HOW TO USE QueryProcessor
         QueryProcessor qp = new QueryProcessor();
-        HashMap<String, HashMap<String, Pair<Integer, Integer, Double, Integer>>> result = qp.processQuery("Style sheets CSS", phraseSearch);
+        HashMap<String, HashMap<String, Pair<Integer, Integer, Double, Integer>>> result = qp.processQuery("Web Site", phraseSearch);
+        System.out.println(result);
         List<String> docsWithAllOccuerence = new ArrayList<>();
         docsWithAllOccuerence.add("input.txt");
-        docsWithAllOccuerence.add("test");
         PhraseSearcher ps = new PhraseSearcher(result, docsWithAllOccuerence, phraseSearch);
         List<String> goldenDocs = ps.getGoldenDocuments();
-        System.out.println("Query:");
-        System.out.println(phraseSearch);
-        System.out.println("Golden documents: ");
+//        System.out.println("Query:");
+//        System.out.println(phraseSearch);
+//        System.out.println("Golden documents: ");
         System.out.println(goldenDocs);
         //        System.out.println(result);
     }
--- src/SearchPackage/PhraseSearcher.java
+++ src/SearchPackage/PhraseSearcher.java
@@ -13,6 +13,13 @@ public class PhraseSearcher {
         return goldenDocuments;
     }
 
+    public static void main(String[] args) {
+        Integer[] a = new Integer[5];
+        for (int i = 0; i < 5; i++) a[i] = i;
+        addToArray(a, 10);
+        System.out.println(Arrays.toString(a));
+    }
+
     public PhraseSearcher(HashMap<String, HashMap<String, Pair<Integer, Integer, Double, Integer>>> invertedIndex, List<String> docsWithAllOccurrence, List<String> query) {
         this.invertedIndex = invertedIndex;
         this.docsWithAllOccurrence = docsWithAllOccurrence;
@@ -24,23 +31,31 @@ public class PhraseSearcher {
     // TODO: implement a function that returns a list of documents in which the words appeared in same order
     private void buildAllOccurrenceDocs() {
         for (String document : docsWithAllOccurrence) {
-            int[] wordIndices = new int[query.size()];
+            Integer[][] wordsIndices = new Integer[query.size()][];
             for (int i = 0; i < query.size(); i++) {
-                int index = invertedIndex.get(query.get(i)).get(document).index;
-                wordIndices[i] = index;
+                List<Integer> index = new ArrayList<>(invertedIndex.get(query.get(i)).get(document).index);
+                wordsIndices[i] = new Integer[index.size()];
+                index.toArray(wordsIndices[i]);
             }
-            boolean isGolden = true;
-            for (int j = 1; j < wordIndices.length; j++) {
-                if (wordIndices[j] - wordIndices[j - 1] != 1) {
-                    isGolden = false;
-                    break;
-                }
+            for (int i = 0; i < query.size(); i++) {
+                addToArray(wordsIndices[i], query.size() - i - 1);
             }
-            if (isGolden)
+            System.out.println(Arrays.deepToString(wordsIndices));
+            List<Integer> resultList = new ArrayList<>(List.of(wordsIndices[0]));
+            for (int i = 0; i < query.size(); i++) {
+                resultList.retainAll(List.of(wordsIndices[i]));
+            }
+            System.out.println(resultList);
+            if (!resultList.isEmpty())
                 goldenDocuments.add(document);
         }
     }
 
+    private static void addToArray(Integer[] list, int value) {
+        for (int i = 0; i < list.length; i++) {
+            list[i] += value;
+        }
+    }
     // TODO: Implement a function that make golden docs at the top of data structure (Built by the ranker)
 
 }
