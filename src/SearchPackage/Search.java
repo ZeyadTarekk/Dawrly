ec0573e1dec7c0c5821d6008287dea2068a86b9d
Zeyad Tarek <zeyad.ta01@gmail.com>
2022-04-28 01:32:40 +0200

    Final search class

---
+++ src/SearchPackage/Search.java
@@ -0,0 +1,42 @@
+package SearchPackage;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+public class Search {
+
+    private List<String> query;
+
+    private PhraseSearcher phraseSearcher;
+    private QueryProcessor qp;
+    private HashMap<String, Pair3<Double, String, String, String>> finalResults;
+
+    private List<String> goldenPages;
+
+    private Ranker rank;
+    private HashMap<String, HashMap<String, Pair<Integer, Integer, Double, Integer>>> result;
+
+    public HashMap<String, Pair3<Double, String, String, String>> searchQuery(String queryToSearch) {
+        goldenPages = new ArrayList<>();
+        query = new ArrayList<>();
+        qp = new QueryProcessor();
+        rank = new Ranker();
+        result = qp.processQuery(queryToSearch, query);
+        finalResults = rank.generateRelevance(result,goldenPages);
+        phraseSearcher = new PhraseSearcher(result,finalResults,goldenPages,query);
+        finalResults = phraseSearcher.getOrderedDocs();
+
+        return finalResults;
+    }
+
+    public static void main(String[] args) {
+        HashMap<String, Pair3<Double, String, String, String>> finalResults;
+        Search ser = new Search();
+        finalResults = ser.searchQuery("JavaScript Tutorial");
+        System.out.println("================================");
+        System.out.println(finalResults);
+        System.out.println("================================");
+    }
+}
+
